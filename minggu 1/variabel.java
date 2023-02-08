b710b9cf1733c8d796b23c9b1b6ac48344236c5d
nabila <124662018+nabilatulazhariyah@users.noreply.github.com>
2023-02-08 13:38:36 +0700

    Create variabel.java

---
+++ minggu 1/variabel.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classscanner;
+
+/**
+ *
+ * @author ADMINISTRAT0R
+ */
+public class variabel {
+    public static void main (String[]args){
+        int nilai = 20;
+        char huruf;
+        huruf = 'A';
+        
+        System.out.println( nilai );
+        System.out.println("isi variabel huruf=" + huruf);
+        
+        
+    }
+}
